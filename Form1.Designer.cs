6b6b236b85fcd40c3c2393815396950e75b2e5ef
Artem <mr.folk18@gmail.com>
2022-06-11 17:03:07 +0400

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace TRBD_LR_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ContractNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CarBrand = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CarModel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Namee = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SurName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.OrderData = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ExecutionData = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.BrandText = new System.Windows.Forms.TextBox();
+            this.ModelText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ContractNumber,
+            this.CarBrand,
+            this.CarModel,
+            this.Namee,
+            this.SurName,
+            this.OrderData,
+            this.ExecutionData});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 142);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(1154, 296);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ContractNumber
+            // 
+            this.ContractNumber.HeaderText = "ContractNumber";
+            this.ContractNumber.MinimumWidth = 7;
+            this.ContractNumber.Name = "ContractNumber";
+            // 
+            // CarBrand
+            // 
+            this.CarBrand.HeaderText = "CarBrand";
+            this.CarBrand.MinimumWidth = 8;
+            this.CarBrand.Name = "CarBrand";
+            // 
+            // CarModel
+            // 
+            this.CarModel.HeaderText = "CarModel";
+            this.CarModel.MinimumWidth = 8;
+            this.CarModel.Name = "CarModel";
+            // 
+            // Namee
+            // 
+            this.Namee.HeaderText = "Name";
+            this.Namee.MinimumWidth = 8;
+            this.Namee.Name = "Namee";
+            // 
+            // SurName
+            // 
+            this.SurName.HeaderText = "SurName";
+            this.SurName.MinimumWidth = 8;
+            this.SurName.Name = "SurName";
+            // 
+            // OrderData
+            // 
+            this.OrderData.HeaderText = "OrderData";
+            this.OrderData.MinimumWidth = 8;
+            this.OrderData.Name = "OrderData";
+            // 
+            // ExecutionData
+            // 
+            this.ExecutionData.HeaderText = "ExecutionData";
+            this.ExecutionData.MinimumWidth = 8;
+            this.ExecutionData.Name = "ExecutionData";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(333, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(833, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "7.1.\tОставить записи, в которых марка и модель автомобиля соответствуют указанным" +
+    ";";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(333, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(833, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "7.2.\tОставить записи, в которых договор был заключён после указанной даты;";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(333, 94);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(833, 34);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "7.3.\tОставить записи, в которых автомобиль ещё находится на этапе ремонта.";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // BrandText
+            // 
+            this.BrandText.Location = new System.Drawing.Point(12, 12);
+            this.BrandText.Name = "BrandText";
+            this.BrandText.Size = new System.Drawing.Size(150, 31);
+            this.BrandText.TabIndex = 4;
+            this.BrandText.Text = "Ford";
+            // 
+            // ModelText
+            // 
+            this.ModelText.Location = new System.Drawing.Point(168, 12);
+            this.ModelText.Name = "ModelText";
+            this.ModelText.Size = new System.Drawing.Size(150, 31);
+            this.ModelText.TabIndex = 5;
+            this.ModelText.Text = "Focus";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(27, 56);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(300, 31);
+            this.dateTimePicker1.TabIndex = 7;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1178, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ModelText);
+            this.Controls.Add(this.BrandText);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox BrandText;
+        private System.Windows.Forms.TextBox ModelText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ContractNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CarBrand;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CarModel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Namee;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SurName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OrderData;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ExecutionData;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Http;
+using System.Data.Common;
+
+//Работу выполнили Волков и Кочаров
+
+namespace TRBD_LR_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+            //строка подключения:
+
+            SqlConnection connection = GetDBConnection();
+            try
+            {
+                connection.Open();
+                //DropTable(connection);
+                //CreateTable(connection);
+                FillTable(connection);
+               // SelectFromTable(connection, "");
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public static SqlConnection GetDBConnection()
+        {
+            string connnectionInfo = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection connection = new SqlConnection(connnectionInfo);
+            return connection;
+        }
+        private void DropTable(SqlConnection connection)
+        {
+            string sql = "DROP TABLE workshop";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = sql;
+            cmd.ExecuteNonQuery();
+        }
+        private void CreateTable(SqlConnection connection)
+        {
+            string sql = "CREATE TABLE workshop(" + "ContractNumber INT PRIMARY KEY ," +
+                "CarBrand NVARCHAR(100)," + "CarModel NVARCHAR(100)," + "Name NVARCHAR(100)," +
+                "SurName NVARCHAR(100)," + "OrderData DATE," + "ExecutionData DATE" + ")";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = sql;
+            cmd.ExecuteNonQuery();
+        }
+        private void FillTable(SqlConnection connection)
+        {
+
+            Random rnd = new Random();
+            string CarBrand, CarModel, Name, SurName, OrderData, ExecutionData;
+            int ContractNumber;
+
+            string[] CarBrands = { "Lada", "Toyota", "Chevrolet", "Nissan", "Ford" };
+            string[,] Models = { { "Granta", "Vesta" },
+                                 { "Rav_4", "Mark_2" },
+                                 { "Camaro", "Corvette" },
+                                 { "GTR_R34", "Qashqai" },
+                                 { "Focus", "Mustang" } };
+            //считавание файлов с фамилиями и именами
+
+            string filename = "C:/Users/ThinkBook/source/repos/TRBD_LR_4/Names.txt";
+            string filename2 = "C:/Users/ThinkBook/source/repos/TRBD_LR_4/Surnames.txt";
+
+            //string[,] Names = new string[51529, 3];
+            //string[,] SurNames = new string[318474, 3];
+            List<string[]> Names = new List<string[]>();
+            List<string[]> SurNames = new List<string[]>();
+            // читаем файл в строку
+            try
+            {
+                string fileText = System.IO.File.ReadAllText(filename);
+                using (StreamReader sr = new StreamReader(filename, Encoding.Default))
+                {
+                    string[] headers = sr.ReadLine().Split(';');//чтобы не брать во внимание заголовок
+                    int k = 0;
+                    while (!sr.EndOfStream)
+                    {
+                        string[] temp = sr.ReadLine().Split(';');
+                        Names.Add(temp);
+                        k++;
+                    }
+                    sr.Close();
+                }
+                fileText = System.IO.File.ReadAllText(filename2);
+                using (StreamReader sr = new StreamReader(filename2, Encoding.Default))
+                {
+                    string[] headers = sr.ReadLine().Split(';');//чтобы не брать во внимание заголовок
+                    int k = 0;
+                    while (!sr.EndOfStream)
+                    {
+                        string[] temp = sr.ReadLine().Split(';');
+                        SurNames.Add(temp);
+                        k++;
+                    }
+                    sr.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            DateTime curDate = DateTime.Now;
+            curDate += new TimeSpan(-10, 0, 0, 0);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                ContractNumber = i;
+                int brand = rnd.Next(0, 5); CarBrand = CarBrands[brand];
+                CarModel = Models[brand, rnd.Next(0, 2)];
+
+                int nameRand = rnd.Next(0, 51529);//второе число - номер последней строки файла с именами
+                Name = Names[nameRand][1];
+                int surnameRand = rnd.Next(0, 318474);
+                SurName = SurNames[surnameRand][1];
+                bool ReCreate = true;
+                while (ReCreate)
+                { //чтобы имена и фамилии совпадали по полу
+                    if (Names[nameRand][2] == SurNames[surnameRand][2] || Names[nameRand][2] == "МЖ" || SurNames[surnameRand][2] == "МЖ")
+                    {
+                        SurName = SurNames[surnameRand][1];
+                        ReCreate = false;
+                    }
+                    else surnameRand = rnd.Next(0, 318474);
+                }
+
+                int day = rnd.Next(0, 30);
+                OrderData = curDate.ToString("yyyy-MM-dd");
+                curDate += new TimeSpan(day, 0, 0, 0);
+                ExecutionData = curDate.ToString("yyyy-MM-dd");
+
+
+                string sql = "Insert into workshop" +
+                    "( CarBrand, CarModel, Name, SurName, OrderData, ExecutionData) " +
+                    $"VALUES (N'{CarBrand}', N'{CarModel}', N'{Name}', N'{SurName}', N'{OrderData}', N'{ExecutionData}')";
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandText = sql;
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void SelectFromTable(SqlConnection connection, string question)
+        {
+            dataGridView1.AutoGenerateColumns = true;
+            string sql = "SELECT * FROM workshop " + question; // ORDER BY ContractNumber
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = sql;
+            List<string[]> data = new List<string[]>();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            DataGridViewTextBoxColumn[] column = new DataGridViewTextBoxColumn[7];
+            string[] header = { "ContractNumber", "CarBrand", "CarModel", "Name", "SurName", "OrderData", "ExecutionData" };
+
+            while (reader.Read())
+            {
+                data.Add(new string[7]);
+                data[data.Count - 1][0] = reader[0].ToString();
+                data[data.Count - 1][1] = reader[1].ToString();
+                data[data.Count - 1][2] = reader[2].ToString();
+                data[data.Count - 1][3] = reader[3].ToString();
+                data[data.Count - 1][4] = reader[4].ToString();
+                data[data.Count - 1][5] = ((DateTime)reader[5]).ToShortDateString();
+                data[data.Count - 1][6] = ((DateTime)reader[6]).ToShortDateString();
+            }
+            reader.Close();
+            connection.Close();
+            foreach (string[] s in data)
+            {
+                dataGridView1.Rows.Add(s);
+            }
+        }
+
+
+
+        //7.1.	Оставить записи, в которых марка и модель автомобиля соответствуют указанным;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            SqlConnection connection = GetDBConnection();
+            connection.Open();
+            string brand = BrandText.Text, model = ModelText.Text;
+
+            SelectFromTable(connection, "WHERE ( CarBrand = N'" + brand + "' AND CarModel = N'" + model + "')");
+        }
+        //7.2.	Оставить записи, в которых договор был заключён после указанной даты;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            SqlConnection connection = GetDBConnection();
+            connection.Open();
+            DateTime Date = dateTimePicker1.Value;
+            SelectFromTable(connection, "WHERE OrderData >= N'" + Date.ToString("yyyy-MM-dd") + "'");
+        }
+        //7.3.	Оставить записи, в которых автомобиль ещё находится на этапе ремонта.
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            SqlConnection connection = GetDBConnection();
+            connection.Open();
+            string curDate = DateTime.Now.ToString("yyyy-MM-dd");
+            SelectFromTable(connection, "WHERE ExecutionData > N'" + curDate + "'");
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
